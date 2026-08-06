05c6a3e03394bcf0eca6e3897cd73502d7f57291
djthaisalem <djthaisalem@gmail.com>
2026-08-06 14:37:30 +0700

    Remove unused legacy Java project

--- lottery-master/src/main/java/core/utils/common/models/paginated/EntityMapper.java
+++
@@ -1,9 +0,0 @@
-package core.utils.common.models.paginated;
-
-/**
- * Mapper which map a domain entity to something else such as DTO
- */
-public interface EntityMapper<D, E> {
-    D map(E entity);
-    void map(E gift, D dto);
-}
